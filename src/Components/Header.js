3cd8361e72588e52b409e9c25b0f19973287d952
muskanmarskole <marskolemuskan36@gmail.com>
2025-04-03 23:57:51 +0530

    css changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -19,8 +19,8 @@ const Header = () => {
         </div>
         <nav className="nav">
           <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="#home">Homes</a></li>
+            <li><a href="#about">Abouts</a></li>
             <li><a href="#services">Services</a></li>
             <li><a href="#contact">Contact</a></li>
             <li><a href="#pages">Pages</a></li>
